9ac704761b236cb954f56207dd51fa51dcab5520
jhonks83 <joao.plbrandao@discente.univasf.edu.br>
2025-03-10 16:27:43 -0300

    Add getter/setter to product class

--- src/main/java/br/com/jota/nao_matriculado/api/entities/Product.java
+++ src/main/java/br/com/jota/nao_matriculado/api/entities/Product.java
@@ -1,5 +1,10 @@
 package br.com.jota.nao_matriculado.api.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Product {
 	private Long id;
 	private String name;
