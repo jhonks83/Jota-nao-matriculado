3bb6855de9546ba9cfb35da0856931f1fbda59ff
jhonks83 <joao.plbrandao@discente.univasf.edu.br>
2025-03-10 16:35:37 -0300

    Add jakarta dependency

--- src/main/java/br/com/jota/nao_matriculado/api/entities/Product.java
+++ src/main/java/br/com/jota/nao_matriculado/api/entities/Product.java
@@ -1,8 +1,12 @@
 package br.com.jota.nao_matriculado.api.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name="products")
 @Setter
 @Getter
 public class Product {
