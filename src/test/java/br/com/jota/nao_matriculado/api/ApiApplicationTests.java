578cd6b916f1e5f643d24e6bb6c4c1761cea8235
jhonks83 <joao.plbrandao@discente.univasf.edu.br>
2025-03-10 16:00:59 -0300

    meu primeiro commit na aula de boa

---
+++ src/test/java/br/com/jota/nao_matriculado/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jota.nao_matriculado.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
